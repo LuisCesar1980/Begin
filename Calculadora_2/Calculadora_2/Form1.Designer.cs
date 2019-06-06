2cee56cae1e898d1ac082bf058f1c985f6fe3560
LuisCesar1980 <51440316+LuisCesar1980@users.noreply.github.com>
2019-06-06 06:39:15 -0300

    Add files via upload

---
+++ Calculadora_2/Calculadora_2/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Calculadora_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSomar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.buttonSubtrair = new System.Windows.Forms.Button();
+            this.buttonMultiplicar = new System.Windows.Forms.Button();
+            this.buttonDividir = new System.Windows.Forms.Button();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            this.buttonSair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSomar
+            // 
+            this.buttonSomar.Location = new System.Drawing.Point(210, 15);
+            this.buttonSomar.Name = "buttonSomar";
+            this.buttonSomar.Size = new System.Drawing.Size(75, 23);
+            this.buttonSomar.TabIndex = 0;
+            this.buttonSomar.Text = "Somar";
+            this.buttonSomar.UseVisualStyleBackColor = true;
+            this.buttonSomar.Click += new System.EventHandler(this.buttonSomar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Location = new System.Drawing.Point(207, 198);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(61, 13);
+            this.labelResultado.TabIndex = 3;
+            this.labelResultado.Text = "..................";
+            // 
+            // buttonSubtrair
+            // 
+            this.buttonSubtrair.Location = new System.Drawing.Point(210, 72);
+            this.buttonSubtrair.Name = "buttonSubtrair";
+            this.buttonSubtrair.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubtrair.TabIndex = 4;
+            this.buttonSubtrair.Text = "Subtrair";
+            this.buttonSubtrair.UseVisualStyleBackColor = true;
+            this.buttonSubtrair.Click += new System.EventHandler(this.buttonSubtrair_Click);
+            // 
+            // buttonMultiplicar
+            // 
+            this.buttonMultiplicar.Location = new System.Drawing.Point(210, 130);
+            this.buttonMultiplicar.Name = "buttonMultiplicar";
+            this.buttonMultiplicar.Size = new System.Drawing.Size(75, 23);
+            this.buttonMultiplicar.TabIndex = 5;
+            this.buttonMultiplicar.Text = "Multiplicar";
+            this.buttonMultiplicar.UseVisualStyleBackColor = true;
+            this.buttonMultiplicar.Click += new System.EventHandler(this.buttonMultiplicar_Click);
+            // 
+            // buttonDividir
+            // 
+            this.buttonDividir.Location = new System.Drawing.Point(311, 15);
+            this.buttonDividir.Name = "buttonDividir";
+            this.buttonDividir.Size = new System.Drawing.Size(75, 23);
+            this.buttonDividir.TabIndex = 6;
+            this.buttonDividir.Text = "Dividir";
+            this.buttonDividir.UseVisualStyleBackColor = true;
+            this.buttonDividir.Click += new System.EventHandler(this.buttonDividir_Click);
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.Location = new System.Drawing.Point(311, 72);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(75, 23);
+            this.buttonLimpar.TabIndex = 7;
+            this.buttonLimpar.Text = "Limpar";
+            this.buttonLimpar.UseVisualStyleBackColor = true;
+            this.buttonLimpar.Click += new System.EventHandler(this.buttonLimpar_Click);
+            // 
+            // buttonSair
+            // 
+            this.buttonSair.Location = new System.Drawing.Point(311, 130);
+            this.buttonSair.Name = "buttonSair";
+            this.buttonSair.Size = new System.Drawing.Size(75, 23);
+            this.buttonSair.TabIndex = 8;
+            this.buttonSair.Text = "Sair";
+            this.buttonSair.UseVisualStyleBackColor = true;
+            this.buttonSair.Click += new System.EventHandler(this.buttonSair_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Número 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Número 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(114, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Resultado:";
+            this.label3.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSair);
+            this.Controls.Add(this.buttonLimpar);
+            this.Controls.Add(this.buttonDividir);
+            this.Controls.Add(this.buttonMultiplicar);
+            this.Controls.Add(this.buttonSubtrair);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonSomar);
+            this.Name = "Form1";
+            this.Text = "Subtrair";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSomar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.Button buttonSubtrair;
+        private System.Windows.Forms.Button buttonMultiplicar;
+        private System.Windows.Forms.Button buttonDividir;
+        private System.Windows.Forms.Button buttonLimpar;
+        private System.Windows.Forms.Button buttonSair;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Calculadora_2/Calculadora_2/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Faço os métodos para as respectivas operações matemáticas
+        public void Somar()
+        {
+            //Código executado se não houver erros
+            try
+            {
+                int valor1, valor2, resultado;
+
+                valor1 = Convert.ToInt32(textBox1.Text);
+                valor2 = Convert.ToInt32(textBox2.Text);
+
+                resultado = valor1 + valor2;
+
+                labelResultado.Text = resultado.ToString();
+            }
+
+            //Se o usuário clicar no botão sem antes digitar um número será disparado 
+            //uma msg de erro
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Digite um número inteiro por favor");
+            }
+        }
+
+        public void Subtrair()
+        {
+            try
+            {
+                int valor1, valor2, resultado;
+
+                valor1 = Convert.ToInt32(textBox1.Text);
+                valor2 = Convert.ToInt32(textBox2.Text);
+
+                resultado = valor1 - valor2;
+
+                labelResultado.Text = resultado.ToString();
+            }
+
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Digite um número inteiro por favor");
+            }
+        }
+
+        public void Multiplicar()
+        {
+            try
+            {
+                int valor1, valor2, resultado;
+
+                valor1 = Convert.ToInt32(textBox1.Text);
+                valor2 = Convert.ToInt32(textBox2.Text);
+
+                resultado = valor1 * valor2;
+
+                labelResultado.Text = resultado.ToString();
+            }
+
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Digite um número inteiro por favor");
+            }
+
+        }
+
+        //Aqui faço a verificação se o número é divisível por 0, se for, 
+        //disparo uma mensagem ao usuário
+        public void Dividir()
+        {
+            try
+            {
+                int valor1, valor2, resultado;
+
+                valor1 = Convert.ToInt32(textBox1.Text);
+                valor2 = Convert.ToInt32(textBox2.Text);
+
+                resultado = valor1 / valor2;
+
+                labelResultado.Text = resultado.ToString();
+            }
+
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Digite um número inteiro por favor");
+            }
+
+            //Se o número for tentar ser dividido por 0 disparo a mensagem de erro e 
+            //limpo os valores
+            catch (System.DivideByZeroException)
+            {
+                MessageBox.Show("Impossível dividir por 0");
+                Limpar();
+            }
+        }
+
+        //Aqui faço o método para limpar os valores
+        public void Limpar()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            labelResultado.Text = "";
+        }
+
+		//Nos botões faço as chamadas aos métodos récem-criados
+        private void buttonSomar_Click(object sender, EventArgs e)
+        {
+            Somar();
+        }
+
+        private void buttonSubtrair_Click(object sender, EventArgs e)
+        {
+            Subtrair();
+        }
+
+        private void buttonMultiplicar_Click(object sender, EventArgs e)
+        {
+            Multiplicar();
+        }
+
+        private void buttonDividir_Click(object sender, EventArgs e)
+        {
+            Dividir();
+        }
+
+        private void buttonLimpar_Click(object sender, EventArgs e)
+        {
+            Limpar();
+        }
+
+        private void buttonSair_Click(object sender, EventArgs e)
+        {
+            DialogResult result; // result vai ser do tipo DialogResult.
+
+            result = MessageBox.Show("Deseja sair do sistema?", "Mensagem do Sistema", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        //No evento KeyPress faço a verificação para permitir apenas números.
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar) == false)
+            {
+                e.Handled = true;
+                MessageBox.Show("Digite um número válido por favor");
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar) == false)
+            {
+                e.Handled = true;
+                MessageBox.Show("Digite um número válido por favor");
+            }
+        }
+
+    }
+}
